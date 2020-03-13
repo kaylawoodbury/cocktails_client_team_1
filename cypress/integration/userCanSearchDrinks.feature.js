bdeccbd6f1c10d1958f517f186b0a28e70dc10ed
Hunter <hmvitous@yahoo.com>
2020-03-13 16:55:56 +0100

    Adds the heroku server url to tests and added axios import in the index.js

--- cypress/integration/userCanSearchDrinks.feature.js
+++ cypress/integration/userCanSearchDrinks.feature.js
@@ -11,7 +11,8 @@ describe("User can search", () => {
   it("successfully by drink name", () => {
     cy.route({
       method: "POST",
-      url: "http://localhost:3000/api/v1/cocktails", //need to update later
+      // url: "http://localhost:3000/api/v1/cocktails",
+      url: "https://cocktails-api-team1.herokuapp.com/api/v1/cocktails", //need to update later
       response: "fixture:margarita_drink_search.json"
     });
     cy.get("#name-search.prompt").type("Margarita");
@@ -24,7 +25,8 @@ describe("User can search", () => {
   it("unsuccessfully by drink name", () => {
     cy.route({
       method: "POST",
-      url: "http://localhost:3000/api/v1/cocktails", //need to update later
+      // url: "http://localhost:3000/api/v1/cocktails",
+      url: "https://cocktails-api-team1.herokuapp.com/api/v1/cocktails", //need to update later
       response: { status: 400, message: ["No drinks were found"] }
     });
     cy.get("#name-search.prompt").type("nnjthdlsndfie");
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as ServiceWorker from "./serviceWorker";
+import axios from "axios"
 
 // axios.defaults.baseURL = "http://localhost:3000/api/"
 axios.defaults.baseURL = "https://cocktails-api-team1.herokuapp.com/"
