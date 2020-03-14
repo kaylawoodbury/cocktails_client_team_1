63e8a0f337ed5113d2adc57ba8ffb29449c4a5aa
Hunter <hmvitous@yahoo.com>
2020-03-14 12:37:57 +0100

    Creates a test for viewing a detail list for a drink and creates a detail button

---
+++ cypress/integration/userCanSeeDrinkDetails.feature.js
@@ -0,0 +1,31 @@
+describe("User can see", () => {
+  beforeEach(() => {
+    cy.server();
+    cy.visit("http://localhost:3001");
+    cy.route({
+      method: "POST",
+      url: "**/cocktails",
+      response: "fixture:margarita_details.json"
+    });
+    cy.get("#name-search.prompt").type("Margarita");
+    cy.get("button")
+      .contains("Search")
+      .click();
+  });
+  it("drink details successfully", () => {
+    cy.get("#details-button").click();
+    cy.get("#details")
+      .invoke("attr", "src")
+      .should(
+        "include",
+        "https://www.thecocktaildb.com/images/media/drink/srpxxp1441209622.jpg"
+      );
+    cy.get("#details").should("contain", "Ice");
+    cy.get("#details").should("contain", "Tequila");
+    cy.get("#details").should("contain", "Lime Juice");
+    cy.get("#details").should("contain", "Cream of coconut");
+    cy.get("#details").should("contain", "2 oz");
+    cy.get("#details").should("contain", "Place all ingredients in a blender");
+    cy.get("#details").should("contain", "Margarita/Coupette glass");
+  });
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,66 @@
-import React from 'react';
-
-function App() {
-  return (
-    <div>
-    </div>
-  );
+import React, { Component } from "react";
+import axios from "axios";
+class App extends Component {
+  state = {
+    query: "",
+    results: [],
+    message: ""
+  };
+  onSubmitFormHandler = async e => {
+    e.preventDefault();
+    let response = await axios.post("/cocktails", {
+      params: {
+        q: e.target.query.value
+      }
+    });
+    if (response.status === 400) {
+      this.setState({
+        message: response.data.message
+      });
+    } else {
+      this.setState({
+        results: response.data.drinks
+      });
+    }
+  };
+  render() {
+    let renderResults;
+    if (Array.isArray(this.state.results) && this.state.results.length > 0) {
+      renderResults = (
+        <div id="result-list">
+          {this.state.results.map(item => {
+            return (
+              <div key={item.id}>
+                <h4>{item.name}</h4>
+                <button id="details-button">Details</button>
+                {item.category} {item.IBA}
+              </div>
+            );
+          })}
+        </div>
+      );
+    } else {
+      renderResults = <div id="message">{this.state.message}</div>;
+    }
+    return (
+      <>
+        <form id="search-by-name" onSubmit={this.onSubmitFormHandler}>
+          <div className="ui search">
+            <input
+              name="query"
+              type="text"
+              id="name-search"
+              className="prompt"
+              placeholder="Search by drink name"
+            ></input>
+            <button id="search" type="submit">
+              Search
+            </button>
+          </div>
+        </form>
+        {renderResults}
+      </>
+    );
+  }
 }
-
 export default App;
